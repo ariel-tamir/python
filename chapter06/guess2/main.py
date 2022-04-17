fa0c828c926fa9b4a99ca432280c9350604c4fba
ariel <ariel.tamir64@gmail.com>
2022-04-17 14:15:06 +0300

    first commit

--- chapter06/guess2/main.py
+++ chapter06/guess2/main.py
@@ -14,13 +14,19 @@ def generate_word():
 def main():
     word = generate_word()
     guess = input("Enter your guess: ")
+    a = 0
     while guess != word:
+        a = a + 1
         if guess > word:
             print("Wrong, your guess is too high")
         else:
             print("Wrong, your guess is too low")
         guess = input("Enter your guess: ")
+
+        if word.startswith(guess):
+            print("This is a good prefix!")
     print("Correct!!! You Win!!!")
+    print("Total of {} guesses".format(a))
 
 
 main()
