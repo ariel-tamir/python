e43f09ec19ca980a9b1b2a81fe1678faf2d7a20b
ariel <ariel.tamir64@gmail.com>
2022-05-19 21:41:04 +0300

    homework

---
+++ homework/002_pay_salary/main.py
@@ -0,0 +1,8 @@
+hours = input("Enter Hours: ")
+rate = input("Enter Rate: ")
+
+hours = float(hours)
+rate = float(rate)
+
+pay = hours * rate
+print("Pay: {}".format(pay))
---
+++ homework/003_temperature/main.py
@@ -0,0 +1,4 @@
+celsius = input("Enter Celsius temperature: ")
+celsius = float(celsius)
+fahrenheit = celsius * 1.8 + 32
+print("converted Fahrenheit temperature: {}".format(fahrenheit))
