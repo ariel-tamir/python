3dddd23ea0b87977b30c7ee57ff1d898a56c9e11
ariel <ariel.tamir64@gmail.com>
2022-05-21 18:55:00 +0300

    homework

--- homework/006_function_quiz/main.py
+++ homework/006_function_quiz/main.py
@@ -1,5 +1,27 @@
 def main():
+    number = input("Enter number: ")
+    numbers = []
 
-    numbers = input("Enter number")
+    while number != "done":
+
+        try:
+            number = int(number)
+            is_number = True
+        except:
+            print("Invalid input")
+            is_number = False
+
+        if is_number:
+            numbers.append(number)
+        number = input("Enter number: ")
+
+    total = sum(numbers)
+    count = len(numbers)
+    average = sum(numbers) / len(numbers)
+    highest = max(numbers)
+    lowest = min(numbers)
+    print("Results are: total: {}, count: {}, average: {}, maximum: {}, minimum: {} ".format(total, count, average,
+                                                                                             highest, lowest))
+
 
 main()
