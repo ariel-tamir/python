15405bf85d7d23832617c5f326c0a4121c25b6d8
ariel <ariel.tamir64@gmail.com>
2022-06-09 18:14:35 +0300

    homework

--- homework/009_board_move/main.py
+++ homework/009_board_move/main.py
@@ -12,38 +12,41 @@ def main():
     x_row = 0
     x_col = 0
     board(x_row, x_col)
+    print()
     move_x = input("Enter your move: ")
     while move_x != "quit":
 
         if move_x == "up":
-            x_row = x_row - 1
-            if x_row < 0:
+            if x_row <= 0:
                 print("Error: unable to move up.")
             else:
+                x_row = x_row - 1
                 board(x_row, x_col)
         elif move_x == "down":
-            x_row = x_row + 1
-            if x_row > 3:
+            if x_row >= 3:
                 print("Error: unable to move down.")
             else:
+                x_row = x_row + 1
                 board(x_row, x_col)
         elif move_x == "right":
-            x_col = x_col + 1
-            if x_col > 3:
+            if x_col >= 3:
                 print("Error: unable to move right.")
             else:
+                x_col = x_col + 1
                 board(x_row, x_col)
         elif move_x == "left":
-            x_col = x_col - 1
-            if x_col < 0:
+            if x_col <= 0:
                 print("Error: unable to move left.")
             else:
+                x_col = x_col - 1
                 board(x_row, x_col)
         else:
             print("Error")
             quit()
 
+        print()
         move_x = input("Enter your move: ")
+    print("Bye bye, have a nice day.")
 
 
 main()
