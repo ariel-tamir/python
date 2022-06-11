14628d1e8ea22db01b05467cde957a46e62f2cd2
ariel <ariel.tamir64@gmail.com>
2022-06-11 19:38:23 +0300

    homework

--- homework/010_casino/main.py
+++ homework/010_casino/main.py
@@ -1,22 +1,40 @@
 import random
 
 
-def new_player():
+def new_player(balance):
     print()
     name = input("Enter your name: ")
     cash = random.randint(50, 100)
     print("Welcome {}, we randomized for your entry amount of {}$.".format(name, cash))
+    balance[name] = cash
+    print(balance)
+
+
+def balances(balance):
+    pass
+
+def play(balance):
+    pass
 
 
 def main():
+    balance = {}
     print("1. Add new player")
     print("2. Show balances")
     print("3. Play")
     print("4. Quit ")
     user_selection = input("Enter your selection: ")
     user_selection = int(user_selection)
-    if user_selection == 1:
-        new_player()
+    while user_selection != 4:
+        if user_selection == 1:
+            new_player(balance)
+        elif user_selection == 2:
+            balances(balance)
+        elif user_selection == 3:
+            play(balance)
+
+        print()
+        user_selection = input("Enter your selection: ")
 
 
 main()
