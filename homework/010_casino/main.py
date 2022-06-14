babf106adc8f40769aad95e9bd6d5f20d69dab25
ariel <ariel.tamir64@gmail.com>
2022-06-14 20:22:51 +0300

    homework

--- homework/010_casino/main.py
+++ homework/010_casino/main.py
@@ -7,22 +7,36 @@ def new_player(balance):
     cash = random.randint(50, 100)
     print("Welcome {}, we randomized for your entry amount of {}$.".format(name, cash))
     balance[name] = cash
-    print(balance)
 
 
 def balances(balance):
-    pass
+    print("The balances are: ")
+    for name, cash in balance.items():
+        print("{} {}$".format(name, cash))
+
 
 def play(balance):
-    pass
+    print()
+    user_name = input("Enter your name: ")
+    user_bet = input("How much money do you want to bet on? ")
+    dice_result = random.randint(1, 6)
+    print("Rolling the dice…… The dice shows {}.".format(dice_result))
+    if 1 <= dice_result <= 3:
+        print("the bet is lost")
+    elif 4 <= dice_result <= 5:
+        print("no profit")
 
 
-def main():
-    balance = {}
+def print_menu():
     print("1. Add new player")
     print("2. Show balances")
     print("3. Play")
     print("4. Quit ")
+
+
+def main():
+    balance = {}
+    print_menu()
     user_selection = input("Enter your selection: ")
     user_selection = int(user_selection)
     while user_selection != 4:
@@ -34,7 +48,9 @@ def main():
             play(balance)
 
         print()
+        print_menu()
         user_selection = input("Enter your selection: ")
+        user_selection = int(user_selection)
 
 
 main()
