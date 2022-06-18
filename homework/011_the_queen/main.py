4cd42d25129f9d9af837d0b989de17fb8d281026
ariel <ariel.tamir64@gmail.com>
2022-06-18 21:03:53 +0300

    homework

--- homework/011_the_queen/main.py
+++ homework/011_the_queen/main.py
@@ -10,4 +10,50 @@ def board(x_row, x_col):
     print("  A B C D E F G H")
 
 
-board(0, 0)
+def get_col(letter):
+    if letter == "A":
+        return 0
+    elif letter == "B":
+        return 1
+    elif letter == "C":
+        return 2
+    elif letter == "D":
+        return 3
+    elif letter == "E":
+        return 4
+    elif letter == "F":
+        return 5
+    elif letter == "G":
+        return 6
+    elif letter == "H":
+        return 7
+
+
+def get_row(number):
+    number = int(number)
+    number = 8 - number
+    return number
+
+
+def target_number(queen_target):
+    letter = queen_target[0]
+    number = queen_target[1]
+    col = get_col(letter)
+    row = get_row(number)
+    return row, col
+
+
+def main():
+    x_row = 7
+    x_col = 0
+    board(x_row, x_col)
+    print()
+    queen_target = input("Enter the queen target destination: ")
+    row, col = target_number(queen_target)
+    if row != x_row:
+        print("Error: {} is not a valid move.".format(queen_target))
+    else:
+        board(row, col)
+
+
+main()
