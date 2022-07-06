fe47611a1a6bbf26c1d4d7ca0dc84032153f07b6
ariel <ariel.tamir64@gmail.com>
2022-07-06 17:31:42 +0300

    homework

---
+++ homework/012_actors/class_actor.py
@@ -0,0 +1,10 @@
+class Actor:
+    def __init__(self, name, birth_year, movies):
+        self.name = name
+        self.birth_year = birth_year
+        self.movies = movies
+
+    def is_in_age_range(from_age,to_age):
+
+
+    def is_in_movie(movie_name):
--- homework/012_actors/main.py
+++ homework/012_actors/main.py
@@ -1,3 +1,17 @@
+from homework.012_actors.class_actor import Actor
+
+def list_actors_by_age():
+
+
+def add_actor():
+
+
+def delete_actor():
+
+
+def delete_movie():
+
+
 def main():
     print("1. List actors by age")
     print("2. Add actor")
@@ -7,6 +21,16 @@ def main():
     print()
     user_selection = input("Enter your selection: ")
     user_selection = int(user_selection)
+    while user_selection != 5:
+        if user_selection == 1:
+            list_actors_by_age()
+        elif user_selection == 2:
+            add_actor()
+        elif user_selection == 3:
+            delete_actor()
+        elif user_selection == 4:
+            delete_movie()
+
 
 
 main()
