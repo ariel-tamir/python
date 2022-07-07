01b6170baaa2398fface4786f26a705d5895abd6
ariel <ariel.tamir64@gmail.com>
2022-07-07 20:19:16 +0300

    homework

--- homework/012_actors/class_actor.py
+++ homework/012_actors/class_actor.py
@@ -4,7 +4,15 @@ class Actor:
         self.birth_year = birth_year
         self.movies = movies
 
-    def is_in_age_range(from_age,to_age):
+    def is_in_age_range(self, from_age, to_age):
+        self.birth_year = 2022 - self.birth_year
+        if from_age <= self.birth_year <= to_age:
+            return True
+        else:
+            return False
 
-
-    def is_in_movie(movie_name):
+    def is_in_movie(self, movie_name):
+        if movie_name in self.movies:
+            return True
+        else:
+            return False
--- homework/012_actors/main.py
+++ homework/012_actors/main.py
@@ -1,9 +1,21 @@
-from homework.012_actors.class_actor import Actor
-
-def list_actors_by_age():
+from class_actor import Actor
 
 
-def add_actor():
+def list_actors_by_age(actor_name):
+    actor_name.is_in_age_range()
+
+
+def add_actor(actors_list):
+    actor_name = input("Enter the actor’s name: ")
+    actor_birth_year = input("Enter the actor’s birth year: ")
+    actor_movies = []
+    actor_movie = input("Enter the actor’s movies, or click Enter to complete: ")
+    actor_movies.append(actor_movie)
+    while actor_movie != '':
+        actor_movie = input("Enter the actor’s movies, or click Enter to complete: ")
+        actor_movies.append(actor_movie)
+    actor = Actor(actor_name, actor_birth_year, actor_movies)
+    actors_list.append(actor)
 
 
 def delete_actor():
@@ -19,6 +31,7 @@ def main():
     print("4. Delete movie")
     print("5. Quit")
     print()
+    actors_list = []
     user_selection = input("Enter your selection: ")
     user_selection = int(user_selection)
     while user_selection != 5:
@@ -30,7 +43,6 @@ def main():
             delete_actor()
         elif user_selection == 4:
             delete_movie()
-
 
 
 main()
